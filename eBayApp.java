f8feda1092bd43d636c048bba4671ce82cfdaedd
fastestmobile <36421925+fastestmobile@users.noreply.github.com>
2018-02-13 16:59:35 +0530

    completed login and logout
    code has to be progressed to cover purchase flow

--- eBayApp.java
+++ eBayApp.java
@@ -5,8 +5,12 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
@@ -16,12 +20,13 @@ public class eBayApp {
 	TestData data=null;
 	DesiredCapabilities caps = null;
 	AppiumDriver<MobileElement> driver = null;
+	ScreenShot ss=null;
 	
 	@BeforeSuite
 	public void setup() throws IOException {
 		
+		ss = new ScreenShot();
 		data = new TestData();
-		
 		caps = new DesiredCapabilities();
 		
 		caps.setCapability("deviceName", data.readData("devicename"));
@@ -43,6 +48,63 @@ public class eBayApp {
 			}
 		
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+	
+	}
+	
+	@AfterSuite
+	public void terminate() {
+		
+		driver.closeApp();
+		driver.close();
+	}
+	
+	@Test (priority=0)
+	public void loginviaMenu() throws IOException {
+		
+		ss.takeScreenShot((AndroidDriver) driver);
+		driver.findElement(By.id("com.ebay.mobile:id/home")).click();
+		
+		ss.takeScreenShot((AndroidDriver) driver);
+		driver.findElement(By.id("com.ebay.mobile:id/textview_sign_out_status")).click();
+		
+		ss.takeScreenShot((AndroidDriver) driver);
+		driver.findElement(By.id("com.ebay.mobile:id/edit_text_username")).click();
+		driver.findElement(By.id("com.ebay.mobile:id/edit_text_username")).sendKeys(data.readData("username"));
+		driver.findElement(By.id("com.ebay.mobile:id/edit_text_password")).click();
+		driver.findElement(By.id("com.ebay.mobile:id/edit_text_password")).sendKeys(data.readData("password"));
+		
+		ss.takeScreenShot((AndroidDriver) driver);
+		driver.findElement(By.id("com.ebay.mobile:id/button_sign_in")).click();
+		
+	}
+	
+	@Test (priority=1)
+	public void logoutAccount() {
+		
+		ss.takeScreenShot((AndroidDriver) driver);
+		driver.findElement(By.id("com.ebay.mobile:id/home")).click();
+		
+		ss.takeScreenShot((AndroidDriver) driver);
+		driver.findElement(By.id("com.ebay.mobile:id/textview_sign_in_status")).click();
+		
+		ss.takeScreenShot((AndroidDriver) driver);
+		driver.findElement(By.id("com.ebay.mobile:id/button_sign_out")).click();
+	}
+	
+	@Test(dependsOnMethods={"loginviaMenu","logoutAccount"})
+	public void loginviaHome() throws IOException {
+		
+		ss.takeScreenShot((AndroidDriver) driver);
+		driver.findElement(By.id("com.ebay.mobile:id/button_sign_in")).click();
+		
+		ss.takeScreenShot((AndroidDriver) driver);
+		driver.findElement(By.id("com.ebay.mobile:id/edit_text_username")).click();
+		driver.findElement(By.id("com.ebay.mobile:id/edit_text_username")).sendKeys(data.readData("username"));
+		driver.findElement(By.id("com.ebay.mobile:id/edit_text_password")).click();
+		driver.findElement(By.id("com.ebay.mobile:id/edit_text_password")).sendKeys(data.readData("password"));
+		
+		ss.takeScreenShot((AndroidDriver) driver);
+		driver.findElement(By.id("com.ebay.mobile:id/button_sign_in")).click();
 		
 	}
 }
